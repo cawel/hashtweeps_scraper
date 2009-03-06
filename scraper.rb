ce546f80bf935ca70fb6771d8b3f470b6d178dcd
Martin Carel <cawel00@yahoo.com>
2009-03-06 13:50:37 -0500

    added comments

--- scraper.rb
+++ scraper.rb
@@ -1,18 +1,20 @@
 require 'rubygems'
 require 'scrubyt'
 
-HASH_KEYWORD = "leadscon"
+HASH_TERM = "leadscon"
 
 data = Scrubyt::Extractor.define do
 	fetch 'http://www.hashtweeps.com/'
-	fill_textfield "term", HASH_KEYWORD
+	fill_textfield "term", HASH_TERM
 	submit
 
+	# build XML structure
 	item "//li[@class='result']" do
 		msg "//div[@class='msg']"
 		link "//div[@class='info']/a[1]/@href"
 		user "//div[@class='info']/a[1]", :generalize => false do
-			page_detail do
+			# this will follow link because it ends with "_detail"
+			page_detail do 
 				profile_info '//ul[@class="about vcard entry-author"]' do
 					full_name "//li//span[@class='fn']"
 					location "//li//span[@class='adr']"
@@ -24,5 +26,6 @@ data = Scrubyt::Extractor.define do
 	end
 end
 
+# dump XML to file
 dump = File.new("output.xml", "w")
 dump.puts data.to_xml
